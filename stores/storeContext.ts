ad08b31207eb847ee232daf63b7bddeede38ad6e
Scarlett Hui <scarletthui225@gmail.com>
2019-12-27 15:27:41 +0800

    remove store

--- stores/storeContext.ts
+++
@@ -1,6 +0,0 @@
-import { createContext } from 'react';
-import { RootStore } from './rootStore';
-
-const RootStoreContext = createContext<RootStore | null>(null);
-
-export default RootStoreContext;
--- types/vendor/next.d.ts
+++ types/vendor/next.d.ts
@@ -1,13 +1,5 @@
 import 'next';
 
-declare module 'next' {
-  import { RootStore } from '../../stores';
-
-  export interface NextPageContext {
-    initialStore: RootStore;
-  }
-}
-
 declare module 'next/config' {
   export interface NextRuntimeConfig {
     TOKEN: string;
